012c9dcaa911e5b1f51dd2331bb609269cbbb6ff
raytieu <ray.tieu@gmail.com>
2022-10-01 14:17:54 -0700

    added social icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <h1 className="text-2xl font-bold">Hello</h1> */}
+      <Home />
     </div>
   );
 }
