0ca1faa24af206d81137a7b1b9eeda261acc2fa3
raytieu <ray.tieu@gmail.com>
2022-11-11 19:48:33 -0800

    edited font-size on contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="bg-phone-bg bg-cover md:bg-gray-200 bg-center w-full h-screen py-20"
+      className="bg-phone-bg bg-cover md:bg-gray-200 bg-center w-full h-screen py-40 md:py-20"
     >
       <AnimationOnScroll animateIn="animate__bounceIn" animateOnce={true}>
         <div className="text-center">
@@ -25,19 +25,21 @@ const Contact = () => {
         animateOnce={true}
       >
         <div className="flex flex-col justify-center items-center mt-20">
-          <p className="text-gray-600 text-5xl mt-12">Raymond Tieu</p>
+          <p className="text-gray-600 text-4xl md:text-5xl mt-12">
+            Raymond Tieu
+          </p>
           <div className="flex flex-col justify-center items-center gap-4 mt-20">
             <div className="flex justify-center gap-4 items-center mt-12">
               <ImLocation2 />
-              <p className="my-4 text-2xl">Irvine, CA</p>
+              <p className="my-4 text-xl md:text-2xl">Irvine, CA</p>
             </div>
             <div className="flex justify-center gap-4 items-center">
               <MdEmail />
-              <p className="my-4 text-2xl">ray.tieu@gmail.com</p>
+              <p className="my-4 text-xl md:text-2xl">ray.tieu@gmail.com</p>
             </div>
             <div className="flex justify-center gap-4 items-center">
               <BsFillPhoneFill />
-              <p className="my-4 text-2xl">626-487-7380</p>
+              <p className="my-4 text-xl md:text-2xl">626-487-7380</p>
             </div>
           </div>
         </div>
